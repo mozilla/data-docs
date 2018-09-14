f4d7e11aeb33667aaadec6aa7533c9075f1c750d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-14 13:02:57 +0000

    Deploy docs-origin.telemetry.mozilla.org to github.com/mozilla/firefox-data-docs.git:gh-pages

--- scripts/link_check.sh
+++ scripts/link_check.sh
@@ -3,7 +3,7 @@
 # If there are any failures along the way, exit nonzero.
 EXIT_CODE=0
 for f in $(find . -name "*.md" | grep -v "_book" | grep -v "node_modules" | sort); do
-    markdown-link-check --config .linkcheck.json $f
+    markdown-link-check --quiet --config .linkcheck.json $f
     RC=$?
     if [ "$RC" -ne "0" ]; then
         EXIT_CODE=$RC
